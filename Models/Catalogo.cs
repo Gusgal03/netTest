d38bd37bdd3763d64d395ca1b1c6dfce3d81ed92
Gustavo Gálvez Bello <ggalvezb@unal.edu.co>
2022-03-09 18:42:41 -0500

    Creating the app
    Created the .net app

---
+++ Models/Catalogo.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace netTest.Models
+{
+    public class Catalogo
+    {
+        [Key] //llave primaria
+        //campos de la base de datos
+        public int IdCatalogo { get; set; }
+    }
+}
