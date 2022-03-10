b1a1602c1df8450aee39baa11d9a8ecacab38201
Gustavo Gálvez Bello <ggalvezb@unal.edu.co>
2022-03-10 11:58:43 -0500

    Db connection
    Changes on json file

---
+++ Models/NetTestContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace netTest.Models
+{
+    public class NetTestContext : DbContext
+    {
+        public NetTestContext(DbContextOptions<NetTestContext> opciones)
+        : base(opciones) //hereda las opciones base a la clase NetTest
+        {
+
+        }
+
+        public DbSet<Catalogo> Catalogo{ get; set; }
+    }
+}
